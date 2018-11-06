cb58f8031e065fa336dcf9dfa9567dc3fc30b222
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-11-06 11:05:35 -0500

    Add the starting version of the GeometricCalculator Java class.

---
+++ src/main/java/labseven/GeometricCalculator.java
@@ -0,0 +1,23 @@
+package labseven;
+
+public class GeometricCalculator {
+
+  public static double calculateSphereVolume(double radius) {
+    double volume;
+    volume = (3 / 4) * (Math.PI) * radius * radius * radius;
+    return volume;
+  }
+
+  public static double calculateTriangleArea(double sideA, double sideB, double sideC) {
+    double area;
+    area = sideA * sideA + sideB * sideC;
+    return area;
+  }
+
+  public static double calculateCylinderVolume(double radius, double height) {
+    double volume;
+    volume = (Math.PI) * radius * radius * radius * height;
+    return volume;
+  }
+
+}
